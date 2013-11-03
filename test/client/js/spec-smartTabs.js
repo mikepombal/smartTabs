03fe78720e0588038faf565f7d2dfd3ef30ca9a4
Mickael Marques <mickaelpombal@gmail.com>
2013-11-03 19:24:56 +0000

    Initial Commit

---
+++ test/client/js/spec-smartTabs.js
@@ -0,0 +1,11 @@
+/*global describe, it*/
+
+(function () {
+    'use strict';
+
+    describe('smartTab', function () {
+        describe('Dom Manipulation', function () {
+            it('Should Test Dom Manipulation');
+        });
+    });
+}());
