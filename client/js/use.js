0afbafeda827f6d1d744e0b14bf9affc353f282b
Mickael Marques <mickaelpombal@gmail.com>
2013-11-08 21:13:47 +0000

    Preparing code to allow defining the CSS of the component

---
+++ client/js/use.js
@@ -0,0 +1,15 @@
+/*global jQuery*/
+
+(function ($) {
+    'use strict';
+
+    var options = [
+        { title: 'Tab 1', templateId: 'template1' },
+        { title: 'Tab 2', templateId: 'template2' },
+        { title: 'Tab 3', templateId: 'template3' }
+    ];
+
+    $('#myTab').smartTabs(options);
+
+
+}(jQuery));
