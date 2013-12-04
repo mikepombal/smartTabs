f1538dcaa59a358b5a275d7e3e8f16fb017b5881
Mickael Marques <mickaelpombal@gmail.com>
2013-12-04 18:38:43 +0000

    Defined summary specs regarding the closing tab functionality

---
+++ test/client/js/spec-remove-tabs.js
@@ -0,0 +1,13 @@
+/*global jQuery, describe, it, beforeEach, afterEach*/
+/*jslint browser:true*/
+
+(function ($) {
+    'use strict';
+
+    describe('Remove Tabs', function () {
+        it('should show an icon to close the tabs if the global configuration areClosable is set to true');
+        it('should allow specifying the closable status for each tab');
+        it('should remove a tab from the list when clicking on its close button');
+    });
+
+}(jQuery));
